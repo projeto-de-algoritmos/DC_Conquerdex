07a77a4c1c7aae3d5c16db7d63e31b9b47808435
Danillo Souza <danillosouza1704@gmail.com>
2021-04-10 21:42:30 -0300

    Arrumado o formato da grid e chamando somente pokemons 1 gen

--- project/src/screens/main/index.js
+++ project/src/screens/main/index.js
@@ -1,5 +1,5 @@
-import pokemonService from '../../services/pokemon';
-import util from '../../utils/util';
+import pokemonService from "../../services/pokemon";
+import util from "../../utils/util";
 import {
   Center,
   Flex,
@@ -13,68 +13,42 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuIcon,
-  MenuCommand,
-  MenuDivider,
-  Grid,
-  GridItem,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
   const [pokemons, setPokemons] = useState([]);
-  pokemonService.getPokemons(0, 50, setPokemons);
+  
+  useEffect(() => {
+    pokemonService.getPokemons(setPokemons);
+  }, []);
 
   const renderType = (typeList) => {
-    // console.log(typeList);
-    return typeList.map((type) => (
-    <Box w="60px" bg={util.getColor(type.type.name)} align="center" borderRadius="10px">
+    return typeList.map((type, index) => (
+      <Box
+        w="60px"
+        bg={util.getColor(type.type.name)}
+        align="center"
+        borderRadius="10px"
+        key={index}
+      >
         <Center>
-            <Text fontFamily="Bangers" textColor="white" fontSize="12px" fontWeight="light">
-              {type.type.name.toUpperCase()}
-            </Text>
+          <Text
+            fontFamily="Bangers"
+            textColor="white"
+            fontSize="12px"
+            fontWeight="light"
+          >
+            {type.type.name.toUpperCase()}
+          </Text>
         </Center>
       </Box>
-    ))
-  }
-  const renderPokemons = (pokemonsList) => {
-    return pokemonsList.map((pokemon)=> (
-      <Box
-      p="10px">
-     <Center
-        w="220px"
-        h="220px"
-        bg="gray.200"
-        borderRadius="10px"
-        border="1px"
-      >
-        <VStack>
-
-        <img src={pokemon.sprites.front_default}/>
-        <Text fontFamily="Bangers" textColor={util.getColor(pokemon.types[0].type.name)}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</Text>
-          <HStack>
-            {renderType(pokemon.types)}
-          </HStack>
-        </VStack>
-      </Center>
-      </Box>
     ));
-  }
-  
-  const renderLine = (pokemons) => {
-    // console.log(pokemons.slice((0*5), (0*5)+4));
-    return [0, 1, 2, 3, 4,5,6,7].map((line) => (
-      <HStack justifyContent="space-around">
-        {renderPokemons(pokemons.slice((line*5), (line*5)+5))}
-      </HStack>
-    ))
-  }
+  };
+
   return (
     <>
       <VStack mb="100px">
-        <HStack w="100vw" bg="tomato" h="8vh">
+        <HStack w="100%" bg="tomato" h="8vh">
           <Menu>
             {({ isOpen }) => (
               <>
@@ -98,7 +72,31 @@ function App() {
         </HStack>
       </VStack>
       <VStack>
-          {renderLine(pokemons)}
+        <SimpleGrid columns={6} justifyContent="space-around">
+          {pokemons.map((pokemon, index) => (
+            <Box p="10px" key={index}>
+              <Center
+                w="220px"
+                h="220px"
+                bg="gray.200"
+                borderRadius="10px"
+                border="1px"
+              >
+                <VStack>
+                  <img src={pokemon.sprites.front_default} />
+                  <Text
+                    fontFamily="Bangers"
+                    textColor={util.getColor(pokemon.types[0].type.name)}
+                  >
+                    {pokemon.name.charAt(0).toUpperCase() +
+                      pokemon.name.slice(1)}
+                  </Text>
+                  <HStack>{renderType(pokemon.types)}</HStack>
+                </VStack>
+              </Center>
+            </Box>
+          ))}
+        </SimpleGrid>
       </VStack>
     </>
   );
--- project/src/services/pokemon.js
+++ project/src/services/pokemon.js
@@ -1,14 +1,12 @@
 import api from './api';
 import axios from 'axios';
-class PokemonService {
-    constructor(){}
-    
-    async getPokemons(offset, limit, setPokemons){
-        let params = `offset=${offset}&limit=${limit}`
+class PokemonService {    
+    async getPokemons(setPokemons){
         let pokemons = [];
-        let response = await api.get(`pokemon?${params}`);
-        for(var pokemon of response.data.results){
-            let poke_info = await axios.get(pokemon.url);
+        let response = await api.get(`generation/1`);
+        for(var pokemon of response.data.pokemon_species){
+            let poke_gen = await axios.get(pokemon.url);
+            let poke_info = await axios.get(poke_gen.data.varieties[0].pokemon.url);
             pokemons.push(poke_info.data);
         }
         setPokemons(pokemons);
